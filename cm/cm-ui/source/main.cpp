78725afe5ec16ab0d42c4f4907c31319fef081ec
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-10-26 19:20:19 -0500

    feat: register MasterController class into the engine context

--- cm/cm-ui/source/main.cpp
+++ cm/cm-ui/source/main.cpp
@@ -1,20 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QQmlContext>
+
+#include <controllers/master-controller.h>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
 
+    qmlRegisterType<cm::controllers::MasterController>(
+                "CM", 1, 0, "MasterController");
+    cm::controllers::MasterController masterController;
+
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/views/MasterView.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    engine.rootContext()->setContextProperty(
+                "masterController", &masterController);
+
+    engine.load(QUrl(QStringLiteral("qrc:/views/MasterView.qml")));
 
     return app.exec();
 }
