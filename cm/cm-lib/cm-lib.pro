5073451ee49497cad34115cab9e57b9a85bf2872
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-10-29 22:17:39 -0500

    feat: add navigation controller class

--- cm/cm-lib/cm-lib.pro
+++ cm/cm-lib/cm-lib.pro
@@ -17,6 +17,7 @@ SOURCES += source/models/client.cpp\
 
 
 HEADERS += source/cm-lib_global.h \
+    source/controllers/navigation-controller.h \
     source/models/client.h \
     source/controllers/master-controller.h
 
---
+++ cm/cm-lib/source/controllers/navigation-controller.h
@@ -0,0 +1,29 @@
+#ifndef NAVIGATIONCONTROLLER_CPP
+#define NAVIGATIONCONTROLLER_CPP
+
+#include <QObject>
+
+#include <cm-lib_global.h>
+#include <models/client.h>
+
+namespace cm {
+namespace controllers {
+
+class CMLIB_EXPORT NavigationController : public QObject
+{
+    Q_OBJECT
+public:
+    explicit NavigationController(QObject* _parent = nullptr)
+        : QObject(_parent)
+    {}
+
+signals:
+    void goCreateClientView();
+    void goDashboardView();
+    void goEditClientView(cm::models::Client* client);
+    void goFindClientView();
+};
+
+}}
+
+#endif // NAVIGATIONCONTROLLER_CPP
