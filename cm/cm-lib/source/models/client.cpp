b8c7937512bc3415b04b38ac479f2f47b18503d7
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-11-16 11:44:23 -0500

    feat: implement Client connection to the other models

--- cm/cm-lib/source/models/client.cpp
+++ cm/cm-lib/source/models/client.cpp
@@ -1,10 +1,45 @@
 #include "client.h"
 
-namespace cm {
-namespace models{
+using namespace cm::data;
 
-Client::Client()
+namespace cm {
+namespace models {
+
+Client::Client(QObject* parent)
+    : Entity(parent, "client")
 {
+    reference = static_cast<StringDecorator*>(
+                addDataItem(new StringDecorator(
+                                this, "reference", "Client Ref")));
+    name = static_cast<StringDecorator*>(
+                addDataItem(new StringDecorator(
+                                this, "name", "Name")));
+    supplyAddress = static_cast<Address*>(
+                addChild(new Address(this), "supplyAddress"));
+    billingAddress = static_cast<Address*>(
+                addChild(new Address(this), "billingAddress"));
+    appointments = static_cast<EntityCollection<Appointment>*>(
+                addChildCollection(
+                    new EntityCollection<Appointment>(this, "appointments")));
+    contacts = static_cast<EntityCollection<Contact>*>(
+                addChildCollection(
+                    new EntityCollection<Contact>(this, "contacts")));
+}
+
+Client::Client(QObject* parent, const QJsonObject& json)
+    : Client(parent)
+{
+    update(json);
+}
+
+QQmlListProperty<Appointment> Client::ui_appointments()
+{
+    return QQmlListProperty<Appointment>(this, appointments->derivedEntities());
+}
+
+QQmlListProperty<Contact> Client::ui_contacts()
+{
+    return QQmlListProperty<Contact>(this, contacts->derivedEntities());
 }
 
 }}
